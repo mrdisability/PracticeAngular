1fae677cefcfbdd27896ab06ebf50217740f59ea
King Ioane <thesamoanppprogrammer@gmail.com>
2022-01-20 00:12:39 +1300

    Added todoslist, adding to TodoDetail

--- src/app/components/todos-list/todos-list.component.ts
+++ src/app/components/todos-list/todos-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todos-list',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosListComponent implements OnInit {
 
-  constructor() { }
+  todos: any;
+  currentTodo: any = null;
+  currentIndex = -1;
+  todo = '';
+  completed = false;
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.retrieveTodos();
+  }
+
+  retrieveTodos(): void {
+    this.todoService.getAll()
+      .subscribe(
+        data => {
+          this.todos = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  refreshList(): void {
+    this.retrieveTodos();
+    this.currentTodo = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveTodo(todo: null, index: number): void {
+    this.currentTodo = todo;
+    this.currentIndex = index;
   }
 
 }
+
+// removeAlltodos(): void {
+  //   this.todoService.deleteAll()
+  //     .subscribe(
+  //       response => {
+  //         console.log(response);
+  //         this.retrievetodos();
+  //       },
+  //       error => {
+  //         console.log(error);
+  //       });
+  // }
+
+  // searchByCompleted(): void {
+  //   this.todoService.findByCompleted(this.completed)
+  //     .subscribe(
+  //       data => {
+  //         this.todos = data;
+  //         console.log(data);
+  //       },
+  //       error => {
+  //         console.log(error);
+  //       });
+  // }
